bdff19f01bb5cfcf10ce3aa8389c567fd179cd09
Cezar Augusto <cezar.agssilva@gmail.com>
2020-07-06 13:47:57 -0300

    Hello Word em Java, Passando argumentos

---
+++ Argumentos.java
@@ -0,0 +1,12 @@
+class Argumentos {
+	
+	public static void main(String[] args){
+
+	System.out.println("Você Digitou " + args[0]);
+
+
+// Quando chamo a execução tenho que passar um argumento que será escrito pelo println  
+
+	}
+
+}
---
+++ HelloWord.java
@@ -0,0 +1,8 @@
+class HelloWord{
+	
+
+	public static void main (String[] args){
+	System.out.print("Olá Mundo, ");
+	System.out.println("Estudando java a todo momento");
+}
+}
